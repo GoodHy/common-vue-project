8821992727971abd1670b83cb35461332b3c0b1f
lihao <591353643@qq.com>
2019-10-25 15:19:36 +0800

    1.更新0.1.2;

--- generators/app/templates/wap/vue.config.js
+++ generators/app/templates/wap/vue.config.js
@@ -1,5 +1,8 @@
 const autoprefixer = require('autoprefixer');
 const pxtorem = require('postcss-pxtorem');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+const IS_PROD = process.env.VUE_APP_MODE === 'production';
 
 const APP_PUBLIC_PATH = process.argv[process.argv.length - 1].startsWith('--')
   ? 'http://websitedn.yiautos.com/' + process.argv[process.argv.length - 1].substring(2)
@@ -21,6 +24,25 @@ module.exports = {
           })
         ]
       }
+    }
+  },
+  configureWebpack: config => {
+    if (IS_PROD) {
+      config.plugins.push(
+        new UglifyJsPlugin({
+          uglifyOptions: {
+            compress: {
+              drop_debugger: true,
+              drop_console: true
+            },
+            sourceMap: false,
+            parallel: true
+          }
+        })
+      );
+      return {};
+    } else {
+      return {};
     }
   }
 };
